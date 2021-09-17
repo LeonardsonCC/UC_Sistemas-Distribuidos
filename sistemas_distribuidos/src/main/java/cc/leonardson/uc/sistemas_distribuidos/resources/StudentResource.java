bf6e320e451bb0cc59b7c1575e122294306639af
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-09-16 21:50:45 -0300

    fim de mais uma aula

--- sistemas_distribuidos/src/main/java/cc/leonardson/uc/sistemas_distribuidos/resources/StudentResource.java
+++ sistemas_distribuidos/src/main/java/cc/leonardson/uc/sistemas_distribuidos/resources/StudentResource.java
@@ -1,13 +1,10 @@
 package cc.leonardson.uc.sistemas_distribuidos.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import cc.leonardson.uc.sistemas_distribuidos.domain.Student;
 import cc.leonardson.uc.sistemas_distribuidos.services.StudentService;
@@ -22,6 +19,21 @@ public class StudentResource {
   @GetMapping("/")
   public List<Student> listAll() {
     return service.listAll();
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Student> listById(@PathVariable(name = "id") Integer id) {
+    return service.listById(id);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteById(@PathVariable(name = "id") Integer id) throws Exception {
+    service.delete(id);
+  }
+
+  @PutMapping("/{id}")
+  public Student editById(@PathVariable(name = "id") Integer id, @RequestBody Student s) throws Exception {
+    return service.edit(id, s);
   }
 
   @PostMapping("/")
--- sistemas_distribuidos/src/main/java/cc/leonardson/uc/sistemas_distribuidos/services/StudentService.java
+++ sistemas_distribuidos/src/main/java/cc/leonardson/uc/sistemas_distribuidos/services/StudentService.java
@@ -1,6 +1,7 @@
 package cc.leonardson.uc.sistemas_distribuidos.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,31 @@ public class StudentService {
 
   public List<Student> listAll() {
     return repository.findAll();
+  }
+
+  public Optional<Student> listById(Integer id) {
+    return repository.findById(id);
+  }
+
+  public void delete(Integer id) throws Exception {
+    Optional<Student> s = this.listById(id);
+    if (s.isPresent()) {
+      repository.delete(s.get());
+    } else {
+      throw new Exception("Student not found");
+    }
+  }
+
+  public Student edit(Integer id, Student s) throws Exception {
+    Optional<Student> studentOptional = this.listById(id);
+    if (!studentOptional.isPresent()) throw new Exception("Student not found");
+    Student student = studentOptional.get();
+    student.setName(s.getName());
+    student.setGrade1(s.getGrade1());
+    student.setGrade2(s.getGrade2());
+    student.setGrade3(s.getGrade3());
+    repository.save(student);
+    return student;
   }
 
   public void addStudent(Student s) {
